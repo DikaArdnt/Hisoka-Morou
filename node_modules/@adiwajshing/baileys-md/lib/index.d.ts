10910ed3a15854277d81159f7e5b0c47dd94c64a
Dika Ardnt <88322331+DikaArdnt@users.noreply.github.com>
2021-12-01 15:18:53 +0700

    Create index.d.ts

---
+++ node_modules/@adiwajshing/baileys-md/lib/index.d.ts
@@ -0,0 +1,8 @@
+import makeWASocket from './Socket';
+export * from '../WAProto';
+export * from './Utils';
+export * from './Types';
+export * from './Defaults';
+export * from './WABinary';
+export declare type WASocket = ReturnType<typeof makeWASocket>;
+export default makeWASocket;
