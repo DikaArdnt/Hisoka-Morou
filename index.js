11f51ec9d0e92d193841b3f776197a7b2aa92ba6
Dika Ardnt <88322331+DikaArdnt@users.noreply.github.com>
2022-03-01 23:26:52 +0700

    YNTKTS

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ require('./config')
 const { default: hisokaConnect, useSingleFileAuthState, DisconnectReason, fetchLatestBaileysVersion, generateForwardMessageContent, prepareWAMessageMedia, generateWAMessageFromContent, generateMessageID, downloadContentFromMessage, makeInMemoryStore, jidDecode, proto } = require("@adiwajshing/baileys")
 const { state, saveState } = useSingleFileAuthState(`./${sessionName}.json`)
 const pino = require('pino')
+const { Boom } = require("@hapi/boom")
 const fs = require('fs')
 const chalk = require('chalk')
 const FileType = require('file-type')
@@ -157,10 +158,19 @@ async function startHisoka() {
 
     hisoka.ev.on('connection.update', async (update) => {
         const { connection, lastDisconnect } = update
+	let reason = lastDisconnect.error ? new Boom(lastDisconnect)?.output.statusCode : 0;
+	    
         if (connection === 'close') {
-            lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut ? startHisoka() : console.log('Koneksi Terputus...')
+            if (reason === DisconnectReason.badSession) { console.log(`Bad Session File, Please Delete Session and Scan Again`); process.exit(); }
+            else if (reason === DisconnectReason.connectionClosed) { console.log("Connection closed, reconnecting...."); startHisoka(); }
+            else if (reason === DisconnectReason.connectionLost) { console.log("Connection Lost from Server, reconnecting..."); startHisoka(); }
+            else if (reason === DisconnectReason.connectionReplaced) { console.log("Connection Replaced, Another New Session Opened, Please Close Current Session First"); process.exit(); }
+            else if (reason === DisconnectReason.loggedOut) { console.log(`Device Logged Out, Please Delete Session and Scan Again.`); process.exit(); }
+            else if (reason === DisconnectReason.restartRequired) { console.log("Restart Required, Restarting..."); startHisoka(); }
+            else if (reason === DisconnectReason.timedOut) { console.log("Connection TimedOut, Reconnecting..."); startHisoka(); }
+            else { console.log(`Unknown DisconnectReason: ${reason}|${connection}`) }
         }
-        console.log('Koneksi Terhubung...', update)
+        console.log('Connected...', update)
     })
 
     hisoka.ev.on('creds.update', saveState)
