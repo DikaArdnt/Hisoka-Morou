cecbb0070d7b965999c0d12e3869f813a423297d
RaffaStore5 <100783386+RaffaStore5@users.noreply.github.com>
2022-03-06 10:29:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,9 +18,9 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','6281382420312','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
+global.owner = ['6285721108765','6285721108765','6285721108765']
+global.premium = ['6285721108765']
+global.packname = 'RAFA'
 global.author = 'WhatsApp Bot'
 global.sessionName = 'hisoka'
 global.prefa = ['','!','.','🐦','🐤','🗿']
