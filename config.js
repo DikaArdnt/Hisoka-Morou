615c29ddded72d517275cce9cdbec84873765718
CAF [CAF] {CAF} (CAF) <92899978+CAF-ID@users.noreply.github.com>
2022-05-11 18:39:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -41,7 +41,7 @@ global.limitawal = {
     free: 100
 }
 global.thumb = fs.readFileSync('./lib/hisoka.jpg')
-global.hisoka = { url: 'https://telegra.ph/file/15209657f9d4f59c7ca1e.mp4' }
+global.visoka = { url: 'https://telegra.ph/file/15209657f9d4f59c7ca1e.mp4' }
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
