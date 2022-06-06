abc2e5790d62590e0adea22a96123fab79fb8a05
NizarBotz <102474700+NizarBotz@users.noreply.github.com>
2022-06-07 00:23:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 /**
-   * Create By Dika Ardnt.
-   * Contact Me on wa.me/6288292024190
-   * Follow https://github.com/DikaArdnt
+   * Create By NizarBotz.
+   * Contact Me on wa.me/6283869436074
+   * Follow https://github.com/NizarBotz 
 */
 
 const fs = require('fs')
@@ -18,11 +18,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','6281382420312','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
+global.owner = ['6283869436074','19593010524','6281994819981']
+global.premium = ['6283869436074']
+global.packname = 'Nizar'
 global.author = 'WhatsApp Bot'
-global.sessionName = 'hisoka'
+global.sessionName = 'nizar'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
