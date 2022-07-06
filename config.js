3a129f01eb62ad468c3193100bd3444b1d178616
xganss <79078097+S4MUR41i@users.noreply.github.com>
2022-07-06 07:00:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,8 +18,8 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','6281382420312','6287721317870','6288292024190']
-global.premium = ['6288292024190']
+global.owner = ['6283167714830','6281382420312','6287721317870','6288292024190','6282136432456']
+global.premium = ['6282136432456']
 global.packname = 'Hisoka Morrow'
 global.author = 'WhatsApp Bot'
 global.sessionName = 'hisoka'
