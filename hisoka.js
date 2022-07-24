fc1e3b070663a83c41bfcd73bc70af8f01100784
GEEZYBOT <108412630+GEEZYBOT@users.noreply.github.com>
2022-07-24 22:45:08 +0700

    Update hisoka.js

--- hisoka.js
+++ hisoka.js
@@ -1,6 +1,6 @@
 /**
-   * Create By Dika Ardnt.
-   * Contact Me on wa.me/6288292024190
+   * Create By Geezy.
+   * Contact Me on wa.me/6289529904752
    * Follow https://github.com/DikaArdnt
 */
 
