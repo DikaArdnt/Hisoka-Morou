91f471d16b16cc1fe03212f98b8f714d7e688d85
Dayy's My name <112631688+Dayrqct@users.noreply.github.com>
2022-09-04 13:19:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,17 +14,17 @@ global.APIs = {
 
 // Apikey Website Api
 global.APIKeys = {
-	'https://zenzapis.xyz': 'Your Key',
+	'https://zenzapis.xyz': '0000',
 }
 
 // Other
-global.owner = ['6283167714830','62895336722950','6287721317870','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
-global.author = 'WhatsApp Bot'
+global.owner = ['6289671377960']
+global.premium = ['6289671377960']
+global.packname = 'Aib'
+global.author = 'Dayrqct BOT'
 global.sessionName = 'hisoka'
-global.prefa = ['','!','.','🐦','🐤','🗿']
-global.sp = '⭔'
+global.prefa = ['!']
+global.sp = '!'
 global.mess = {
     success: '✓ Success',
     admin: 'Fitur Khusus Admin Group!',
