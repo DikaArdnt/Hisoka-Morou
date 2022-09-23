481d1ee7e63d38a3e7d8b78c528d10d94be2cc76
NewMybot1 <114190263+NewMybot1@users.noreply.github.com>
2022-09-24 00:33:54 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 /**
    * Create By Dika Ardnt.
-   * Contact Me on wa.me/6288292024190
+   * Contact Me on wa.me/923183098772
    * Follow https://github.com/DikaArdnt
 */
 
@@ -18,8 +18,8 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','62895336722950','6287721317870','6288292024190']
-global.premium = ['6288292024190']
+global.owner = ['923183098772','923183098772','923183098772','923183098772']
+global.premium = ['923183098772']
 global.packname = 'Hisoka Morrow'
 global.author = 'WhatsApp Bot'
 global.sessionName = 'hisoka'
