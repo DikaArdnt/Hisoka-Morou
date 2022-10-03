04b062ab3c3b3ef8b3e39441fb1a54b2858ba66b
Erwimgg00 <114753798+Erwimgg00@users.noreply.github.com>
2022-10-03 20:12:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','62895336722950','6287721317870','6288292024190']
+global.owner = ['6289520951385','6289520951385','6289520951385','6289520951385']
 global.premium = ['6288292024190']
 global.packname = 'Hisoka Morrow'
 global.author = 'WhatsApp Bot'
