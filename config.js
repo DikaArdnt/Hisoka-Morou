6e183f76d0bacacffdf904d85d57fa6b6b023376
Taufiqurrohman22 <86914986+Taufiqurrohman22@users.noreply.github.com>
2022-12-08 15:19:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,11 +18,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','62895336722950','6287721317870','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
+global.owner = ['6283822205585','6283822205585','6283822205585','6283822205585']
+global.premium = ['6283822205585']
+global.packname = 'Iavqmi'
 global.author = 'WhatsApp Bot'
-global.sessionName = 'hisoka'
+global.sessionName = 'Session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
