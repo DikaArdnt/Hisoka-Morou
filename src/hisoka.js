87576d0be7835ca89472d37439c6ac935feb6bc3
Dika Ardnt <88322331+DikaArdnt@users.noreply.github.com>
2023-05-07 10:58:21 -0700

    Update hisoka.js

--- src/hisoka.js
+++ src/hisoka.js
@@ -28,7 +28,7 @@ async function start() {
     readCommands()
 
     const content = await database.read()
-    if (content.data) {
+    if (!content && Object.keys(content).length === 0) {
         global.db = {
             users: {},
             groups: {},
