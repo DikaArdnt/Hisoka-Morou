edc1824452e663fb5a8ca79f719ef08b5350abfe
Dika Ardnt <88322331+DikaArdnt@users.noreply.github.com>
2023-05-08 18:44:23 -0700

    Update lib.database.js

--- src/lib/lib.database.js
+++ src/lib/lib.database.js
@@ -10,7 +10,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 
 if (/mongo/.test(global.options.URI)) {
-     database = class MongoDB {
+   database = class MongoDB {
       constructor(url) {
          this.url = url
 
@@ -18,7 +18,8 @@ if (/mongo/.test(global.options.URI)) {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             keepAlive: true,
-            keepAliveInitialDelay: 30000
+            keepAliveInitialDelay: 30000,
+            // timeout: 30000
          }
          this.connection = this.url || global.options.URI
          this.model = {
@@ -26,68 +27,71 @@ if (/mongo/.test(global.options.URI)) {
          }
          this.data = {}
       }
-       
-       read = async () => {
-          mongoose.connect(this.connection, {
-             ...this.options
-          })
-          try {
-             const schemaData = new mongoose.Schema({
-                data: {
-                   type: Object,
-                   required: true,
-                   default: {}
-                }
-             })
-             this.model.database = mongoose.model('data', schemaData)
-          } catch {
-             this.model.database = mongoose.model('data')
-          }
-          this.data = await this.model.database.findOne({})
-          if (!this.data) {
-             (new this.model.database({
-                data: {}
-             })).save()
-             this.data = await this.model.database.findOne({})
-             return this.data = this.data?.data
-          } else return this.data
-       }
- 
-       write = async (data) => {
-          const obj = data ? data : global.db
-          if (this.data && !this.data.data) return (new this.model.database({
-             data: obj
-          })).save()
-          this.model.database.findById(this.data._id, (error, document) => {
-             if (error) return
-             if (!document.data) document.data = {}
-             document.data = global.db
-             document.save()
-          })
-       }
-    }
+
+      read = async () => {
+         mongoose.connect(this.connection, {
+            ...this.options
+         })
+         try {
+            const schemaData = new mongoose.Schema({
+               data: {
+                  type: Object,
+                  required: true,
+                  default: {}
+               }
+            })
+            this.model.database = mongoose.model('data', schemaData)
+         } catch {
+            this.model.database = mongoose.model('data')
+         }
+         this.data = await this.model.database.findOne({})
+         if (!this.data) {
+            (new this.model.database({
+               data: {}
+            })).save()
+            this.data = await this.model.database.findOne({})
+            return this.data = this.data?.data
+         } else return this.data
+      }
+
+      write = async (data) => {
+         const obj = data ? data : global.db
+         if (this.data && !this.data.data) return (new this.model.database({
+            data: obj
+         })).save()
+         const document = await this.model.database.findById(this.data._id)
+         if (!document.data) document.data = {}
+         document.data = obj
+         document.save()
+      }
+   }
 } else if (/json/.test(global.options.URI)) {
-      database = class Database {
-        file = fs.existsSync(global.options.URI) ? path.resolve(global.options.URI) : path.join(__dirname, "..", "..", "temp", "database.json")
-        
-        data = {}
+   database = class Database {
+      constructor(URI) {
+         this.URI = URI
+      }
+      file = fs.existsSync(global.options.URI) ? path.resolve(global.options.URI) : path.join(__dirname, "..", "..", "temp", this.URI || global.options.URI)
 
-        read = async() => {
-            try {
-                return this.data = fs.existsSync(this.file) ? JSON.parse(fs.readFileSync(this.file)) : {}
-            } catch(e) {
-                console.error(e)
-                return this.data = {}
-            }
-        }
+      data = {}
 
-        write = async() => {
-            let dirname = path.dirname(this.file)
-            if (fs.existsSync(dirname)) await fs.mkdir(dirname, { recursive:true })
-            await fs.writeFile(this.file, JSON.stringify(this.data))
-            return this.file
-        }
-    }
+      read = async () => {
+         try {
+            return this.data = { data: fs.existsSync(this.file) ? JSON.parse(fs.readFileSync(this.file)) : {} }
+         } catch (e) {
+            console.error(e)
+            return this.data = {}
+         }
+      }
+
+      write = async (data) => {
+         let obj = data ? data : global.db
+         let dirname = path.dirname(this.file)
+         if (!fs.existsSync(dirname)) await fs.mkdirSync(dirname, { recursive: true })
+         this.data = { data: obj }
+         await fs.writeFileSync(this.file, JSON.stringify(this.data, null, 2))
+         return this.file
+      }
+   }
 }
 
 
