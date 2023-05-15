7125ab27864d03372c4b650e03f325f61c3c5d3f
Dika Ardnt <88322331+DikaArdnt@users.noreply.github.com>
2023-05-15 15:13:01 +0700

    Update yta.js

--- src/commands/download/yta.js
+++ src/commands/download/yta.js
@@ -8,7 +8,7 @@ export default {
         mess("wait", m)
         let request = await (new api('xzn')).get('/api/y2mate', { url: Func.isUrl(m.body)[0] })
         if (request.data?.err) return mess('error', m)
-        let { id: vid, title, links, a } = request.data
+        let { id: vid, title, links, a: channel } = request.data
         let a = links.audio[Object.keys(links.audio)[0]]
         let b = await Func.fetchJson(a.url)
         let { size, data } = await Func.getFile(b.dlink)
@@ -16,7 +16,7 @@ export default {
         if (size >= limit.download.premium && !m.isVIP) return mess("dlPremium", m, { extra: `, please download manually ${b.dlink}` })
         if (size >= limit.download.VIP) return mess("dlVIP", m, { extra: `, please download manually ${b.dlink}` })
         let msg = await hisoka.sendMedia(m.from, thumbnail, m, { caption: title })
-        hisoka.sendMessage(m.from, { document: data, mimetype: "audio/mpeg", fileName: `${title} ${a}.mp3` }, { quoted: msg })
+        hisoka.sendMessage(m.from, { document: data, mimetype: "audio/mpeg", fileName: `${title} - ${channel}.mp3` }, { quoted: msg })
     },
     isLimit: true
 }
