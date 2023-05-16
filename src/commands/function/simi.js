472609141c3f3ecc11e6dece897e877d5468a494
Hisoka <95246137+Hisoka775@users.noreply.github.com>
2023-05-16 19:15:48 +0800

    hehe

--- src/commands/function/simi.js
+++ src/commands/function/simi.js
@@ -1,6 +1,7 @@
-export default async function({ m }) {
-    if (m.isGroup && !global.db.users[m.from]?.simi && m.fromMe) return
-    let text = await (new api('xzn')).get('/api/simi', { text: m.body })
-    if (text.status !== 200) return m.reply('Sorry, I do not understand')
-    m.reply(text.data.result)
+export default async function ({ m }) {
+    if (!m.isGroup && global.db.users[m.from]?.simi && !m.fromMe) {
+        let text = await (new api('xzn')).get('/api/simi', { text: m.body })
+        if (text.status !== 200) return m.reply('Sorry, I do not understand')
+        m.reply(text.data.result)
+    }
 }
