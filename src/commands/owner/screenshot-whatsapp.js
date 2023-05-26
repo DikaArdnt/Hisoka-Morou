3c621d6ee20c28e174b9c98695df2c6f741263e5
Dika Ardnt <88322331+DikaArdnt@users.noreply.github.com>
2023-05-27 02:05:10 +0800

    module change

--- src/commands/owner/screenshot-whatsapp.js
+++ src/commands/owner/screenshot-whatsapp.js
@@ -5,7 +5,7 @@ export default {
     desc: "Screenshot homepage whatsapp web",
     execute: async({ hisoka, m }) => {
         await hisoka.pupPage.setViewportSize({ width:961, height: 2000 })
-        let media = await hisoka.pupPage.screenshot()
+        let media = await hisoka.mPage.screenshot()
         await hisoka.sendMessage(m.from, media, { quoted: m })
     },
     isOwner: true
