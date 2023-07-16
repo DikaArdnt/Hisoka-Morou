83fb5eb153e96cd33821f7c4ce1b1e0318cd03fa
Dika Ardnt <88322331+DikaArdnt@users.noreply.github.com>
2023-07-16 21:44:00 +0700

    Update owner.js

--- src/commands/main/owner.js
+++ src/commands/main/owner.js
@@ -2,9 +2,9 @@ export default {
     name: 'owner',
     aliases: ['develer'],
     type: 'main',
-    execute: async({ hisoka, m }) => {
+    execute: async({ hisoka, m, config }) => {
         let contacts = []
-        for (const contact of global.owner) {
+        for (const contact of config.options.owner) {
             contacts.push(await hisoka.getContactById(contact + '@c.us'))
         }
         
